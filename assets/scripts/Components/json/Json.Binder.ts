d0007ac4b972952d5b0110f97e7da7143248c920
pTSern <tsern.dev@gmail.com>
2026-07-21 16:55:49 +0700

    push

--- assets/scripts/Components/json/Json.Binder.ts
+++ assets/scripts/Components/json/Json.Binder.ts
@@ -9,11 +9,36 @@ export class Json_Binder extends Component {
     @property({ type: JsonAsset })
     asset: JsonAsset = null
 
+    @property({  })
+    get log() { return false }
+    set log(x) {
+        if(!x) return;
+        console.log(JSON.stringify(this.extract(), null, 4))
+    }
+
     @property({ type: Helper_Param_Creator })
     params: Helper_Param_Creator[] = []
 
     extract() {
+        const _arr = this.params.map(_ => _.extract(true));
 
+        return _arr;
+    }
+
+    protected start(): void {
+        this._read();
+    }
+
+    protected _read() {
+        if(!this.asset) return;
+
+        const _json = this.asset.json;
+        if(!_json) return;
+
+        if(!Array.isArray(_json)) return;
+        const _out = Helper_Param_Creator.read(_json);
+
+        console.log("?? OUT >>", _out);
     }
 
 }
