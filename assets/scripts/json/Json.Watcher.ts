8585420e05be0b504e288218e1327fdf4b665bf5
pTSern <tsern.dev@gmail.com>
2026-08-17 11:07:25 +0700

    push

--- assets/scripts/json/Json.Watcher.ts
+++ assets/scripts/json/Json.Watcher.ts
@@ -1,18 +1,25 @@
-import { _decorator, Asset, Component } from "cc";
+import { _decorator, Component, JsonAsset } from "cc";
+import { pEngine } from "db://pts-core/scripts/utils";
+import { editor_property } from "db://pts-core/scripts/utils/pClass";
 
 const { ccclass, property } = _decorator
 
 
 @ccclass("Json_Watcher")
 export class Json_Watcher extends Component {
-    @property({ type: Asset })
-    protected _target: Asset = null
-    @property({ type: Asset })
-    get target(): Asset { return this._target }
+    @property({ type: JsonAsset })
+    protected _target: JsonAsset = null
+    @property({ type: JsonAsset })
+    get target(): JsonAsset { return this._target }
     set target(x) {
         this._target = x;
         this._valid();
     }
+
+    @editor_property()
+    get __event() { return this._target ? pEngine.Json.event.previewer(this._target) : null }
+    @editor_property()
+    get __param() { return this._target ? pEngine.Json.param.previewer(this._target) : null }
 
     protected _valid() {
         if(!this._target) return
